13cb9e0aed1e76989837167c55fae7eb08c2208d
Ganesh <ganesh.pathak@live.com>
2021-05-11 17:06:42 +0400

    Updates

---
+++ precache-manifest.914cfa70002e02964ed62110700c93a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b30c86230f95eeff9630799a1c0069d",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "3094a758bbb71f71de42",
+    "url": "/helloworld/static/js/2.9f077c59.chunk.js"
+  },
+  {
+    "revision": "a257cb295dbee7296430",
+    "url": "/helloworld/static/js/main.aad7e4d5.chunk.js"
+  },
+  {
+    "revision": "7345ca8677c769cc30fe",
+    "url": "/helloworld/static/js/runtime~main.1befb155.js"
+  },
+  {
+    "revision": "70b4c10c29e034105eda5ff50d28135b",
+    "url": "/helloworld/static/media/ganesh.70b4c10c.jpeg"
+  }
+]);
